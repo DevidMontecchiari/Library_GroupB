d89d5b4702f509845be7837b1c15522639c85f67
Devid Montecchiari <47670152+DevidMontecchiari@users.noreply.github.com>
2025-12-03 19:29:55 +0000

    merge with existing solution

--- main.py
+++ main.py
@@ -1,45 +1,86 @@
 from fastapi import FastAPI, HTTPException
-import json
 from pathlib import Path
+import json
 
-app = FastAPI(title="Pizzeria API")
-
-ORDERS_FILE = Path("orders.json")
-
-
-def load_orders():
-    if not ORDERS_FILE.exists():
-        return []
-    with open(ORDERS_FILE, "r") as f:
-        return json.load(f)
-
-def save_orders(orders):
-    with open(ORDERS_FILE, "w") as f:
-        json.dump(orders, f, indent=4)
-
+app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"message": "Welcome to the Pizzeria API!"}
+def home():
+    return {"message": "Hello Group B!"}
+
+DB_FILE = Path("library_db.json")
+
+
+def load_data():
+    if not DB_FILE.exists():
+        return {
+            "students": [],
+            "books": [],
+            "loans": []
+        }
+
+    with open(DB_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
 
 
 # ---------------------------
-# READ – Get a single order
+# READ – Students
 # ---------------------------
-@app.get("/orders/{order_id}")
-def get_order(order_id: int):
-    orders = load_orders()
 
-    for order in orders:
-        if order["id"] == order_id:
-            return order
+@app.get("/students/{student_id}")
+def get_student(student_id: int):
+    data = load_data()
 
-    raise HTTPException(status_code=404, detail="Order not found")
+    for student in data["students"]:
+        if student["StudentID"] == student_id:
+            return student
+
+    raise HTTPException(status_code=404, detail="Student not found")
+
+
+@app.get("/students")
+def list_students():
+    data = load_data()
+    return data["students"]
 
 
 # ---------------------------
-# READ – Get all orders
+# READ – Books
 # ---------------------------
-@app.get("/orders")
-def list_orders():
-    return load_orders()
+
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    data = load_data()
+
+    for book in data["books"]:
+        if book["BookID"] == book_id:
+            return book
+
+    raise HTTPException(status_code=404, detail="Book not found")
+
+
+@app.get("/books")
+def list_books():
+    data = load_data()
+    return data["books"]
+
+
+# ---------------------------
+# READ – Loans
+# ---------------------------
+
+@app.get("/loans/{loan_id}")
+def get_loan(loan_id: int):
+    data = load_data()
+
+    for loan in data["loans"]:
+        if loan["LoanID"] == loan_id:
+            return loan
+
+    raise HTTPException(status_code=404, detail="Loan not found")
+
+
+@app.get("/loans")
+def list_loans():
+    data = load_data()
+    return data["loans"]
